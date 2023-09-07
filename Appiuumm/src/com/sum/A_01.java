dd97664098bf024bcd5e687079e5b493edd1292c
radhey355 <saran35515@gmail.com>
2023-09-07 11:17:16 +0530

    Change Name

---
+++ Appiuumm/src/com/sum/A_01.java
@@ -0,0 +1,21 @@
+package com.sum;
+
+public class A_01 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		
+		System.out.println("=======");
+		
+		int c=20;
+		int d=25;
+		System.out.println("c+d");
+		System.out.println("not YET..");
+				
+		
+	}
+
+}
